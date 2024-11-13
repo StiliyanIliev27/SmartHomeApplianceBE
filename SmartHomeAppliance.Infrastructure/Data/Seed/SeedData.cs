2483b9426515f79deca6b7c35ab7006ba00a4782
StiliyanIliev27 <stiliyaniliev2705@gmail.com>
2024-11-14 00:32:45 +0200

    Seed data fixed syntax error

--- SmartHomeAppliance.Infrastructure/Data/Seed/SeedData.cs
+++ SmartHomeAppliance.Infrastructure/Data/Seed/SeedData.cs
@@ -26,7 +26,7 @@ namespace SmartHomeAppliance.Infrastructure.Data.Seed
                 NormalizedEmail = "admin@smarthomeapp.bg".ToUpper(),
                 FirstName = "Admin",
                 LastName = "Adminov",
-                ProfilePictureUrl = "https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                ProfilePictureUrl = "https://plus.unsplash.com/premium_photo-1689530775582-83b8abdb5020?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
                 PasswordHash = hasher.HashPassword(null!, "admin123")
             };
 
